d67f87ba5755ac720978d42c3efd84289d09e9df
Geeth Apuroop <74822141+Geethb4u@users.noreply.github.com>
2020-11-21 20:35:18 +0530

    Update stringcode.py

--- stringcode.py
+++ stringcode.py
@@ -1,23 +1,43 @@
-def maxdistchar(str,n):
-    count=[0]*num_of_chars
-    for i in range(n):
-        count[ord(str[i])]+=1
-    maxdist=0
-    for i in range(num_of_chars):
-        if(count[i]!=0):
-            maxdist+=1
-    return maxdist
-def smallest_substring(str):
-    n=len(str)
-    maxdist=maxdistchar(str):
-    minl=n
-    
-    for i in range(n):
-        for j in range(n):
-            sub=str[i:j]
-            sublen=len(sub)
-            subdistchar = maxdistchar(sub,sublen)
-    return minl
-if __name__=="__main__":
-l=smallest_substring(str):
-print(l)
+ 
+NO_OF_CHARS = 256
+
+def max_distinct_char(str, n): 
+	count = [0] * NO_OF_CHARS 
+	
+	for i in range(n): 
+		count[ord(str[i])] += 1
+	
+	max_distinct = 0
+	for i in range(NO_OF_CHARS): 
+		if (count[i] != 0): 
+			max_distinct += 1	
+	
+	return max_distinct 
+
+def smallesteSubstr_maxDistictChar(str): 
+
+	n = len(str)	
+	max_distinct = max_distinct_char(str, n) 
+	minl = n	
+ 
+	for i in range(n): 
+		for j in range(n): 
+			subs = str[i:j] 
+			subs_lenght = len(subs) 
+			sub_distinct_char = max_distinct_char(subs,subs_lenght) 
+			
+			if (subs_lenght < minl and max_distinct == sub_distinct_char): 
+				minl = subs_lenght 
+
+	return minl 
+
+
+if __name__ == "__main__": 
+	
+
+	str = input("")
+	
+	l = smallesteSubstr_maxDistictChar(str); 
+	print(l) 
+
+
